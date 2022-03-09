622abe82987472dafee5f4d98575f63b459f68f3
Giulia Bianca <86010339+GiuBi2@users.noreply.github.com>
2022-03-09 21:16:57 +0000

    criei função inserir

--- models/autor.js
+++ models/autor.js
@@ -5,19 +5,22 @@ class Autor{
     const connect = await db.connect();
     return await connect.query("select * FROM autores");
   }
-  static inserir(data){
+  
+  static async inserir(data){
     const connect = await db.connect();
-    const sql = "insert into autores (nome,sobrenome, data_nascimento)values($1, $2, $3)";
-    const values = [data.nome, data.sobrenome, data.datanascimento]
+    const sql = "insert into autores (nome,sobrenome, data_nasc)values($1, $2, $3)";
+    const values = [data.nome, data.sobrenome, data.data_nasc]
     return await connect.query(sql, values);
   }
-  static atualizar(){
+  
+  static async atualizar(){
      const connect = await db.connect();
     
   }
-  static deletar(){
+  static async deletar(){
      const connect = await db.connect();
   }
 } 
 
 module.exports = Autor;
+
