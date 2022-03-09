5c6ed78869ee5f6fa07595aee0845a8d3978f5dc
Giulia Bianca <86010339+GiuBi2@users.noreply.github.com>
2022-03-09 20:52:48 +0000

    inseri autores no banco dados

--- models/autor.js
+++ models/autor.js
@@ -5,11 +5,18 @@ class Autor{
     const connect = await db.connect();
     return await connect.query("select * FROM autores");
   }
-  static inserir(){
+  static inserir(data){
+    const connect = await db.connect();
+    const sql = "insert into autores (nome,sobrenome, data_nascimento)values($1, $2, $3)";
+    const values = [data.nome, data.sobrenome, data.datanascimento]
+    return await connect.query(sql, values);
   }
   static atualizar(){
+     const connect = await db.connect();
+    
   }
   static deletar(){
+     const connect = await db.connect();
   }
 } 
 
