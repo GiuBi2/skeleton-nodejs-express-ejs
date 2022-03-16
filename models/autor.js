d02f81ce86e0411143850af6bfc8812dd309b5c7
Giulia Bianca <86010339+GiuBi2@users.noreply.github.com>
2022-03-16 20:41:10 +0000

    criei método de atualizar

--- models/autor.js
+++ models/autor.js
@@ -13,14 +13,19 @@ class Autor{
     return await connect.query(sql, values);
   }
   
-  static async atualizar(){
+  static async atualizar(data, id){
      const connect = await db.connect();
-    
+    const sql = "UPDATE autores SET nome=$1, sobrenome=$2, data_nasc=$3 WHERE id=$4";
+     const values = [data.nome, data.sobrenome, data.data_nasc, id]
+    return await connect.query(sql, values);
   }
-  static async deletar(){
+  
+  static async deletar(id){
      const connect = await db.connect();
+    const sql = "DELETE FROM autores WHERE id=$1 "
+    return await connect.query(sql, id);
   }
 } 
 
 module.exports = Autor;
-
+  
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,21 @@ router.get('/', async function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/', async function(req, res, next) {
+  const autores = await Autor.deletar(id);
+  res.json(autores. rows);
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/', async function(req, res, next) {
+  const autores = await Autor.atualizar(id);
+  res.json(autores. rows);
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/', async function(req, res, next) {
+  const autores = await Autor.inserir();
+  res.json(autores. rows);
+  res.render('index', { title: 'Express' });
+});
 module.exports = router;
